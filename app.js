5c13e3457cb3a1cd72b4d43c5a5781834c9be3ed
anne-isabelle <petito.anne-isabelle@neuf.fr>
2016-08-29 17:49:47 +0200

    deuxième commit

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-var bot = angular.module("botApp",[]);
+var bot = angular.module("botApp",[]);		//création d'un nouveau module
+var toto = angular.module("botApp");		//récupération du module botApp qui a déjà été crée
 
 bot.controller("botController", function($scope){
 	$scope.hello = "Hello";
 });
+
+
+bot.controller("dictionnaryCtrl", function($scope){
+	$scope.sentences = [{
+		word: "Le gras",
+		response: "c'est la vie"
+		},
+		{word: "Je suis victime",
+		response: "des colifichets"},
+		{word: "Au revoir",
+		response: "Bienvenue"},
+		{word: "Allô",
+		response: "A l'huile"}];
+});
+
+bot.controller("suppressCtrl", function($scope){
+	$scope.sendClick = function(){
+		alert("je vais supprimer");
+	};
+});
+
